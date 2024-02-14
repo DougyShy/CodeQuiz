d2d618a2584877cd62e694d37eef94da28cc45cf
Doug Scheible <doug_scheible@yahoo.com>
2024-02-14 03:25:40 -0600

    Got buttons to work thanks to jQuery

--- code-quiz.js
+++ code-quiz.js
@@ -1,9 +1,12 @@
 var questionElement = document.querySelector(".question");
 var timerElement = document.querySelector(".time-left");
+var buttonStartElement = $("#button-start");
 var buttonOneElement = $("#button-one");
 var buttonTwoElement = $("#button-two");
 var buttonThreeElement = $("#button-three");
 var buttonFourElement = $("#button-four");
+
+var answersElement = $('ul');
 
 var instructionsText = "<h1>" + "Coding Quiz Challenge</h1><h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorect answers will penalize your score/time by ten seconds!</H2>";
 
@@ -27,17 +30,31 @@ function resetInstructions() {
     // REMOVE: timerElement.innerHTML="Time: " + "59";
 
     // Reset buttons
-    buttonTwoElement.css('display', 'none')
+    buttonStartElement.css('display', 'inline-block');
+    buttonOneElement.css('display', 'none');
+    buttonTwoElement.css('display', 'none');
     buttonThreeElement.css('display', 'none')
     buttonFourElement.css('display', 'none');
     timerElement.innerHTML="Time: 0";
 }
 
-
+function toggleAnswerButtons(state) {
+    if (state == "on") {
+        buttonStartElement.css('display', 'none');
+        buttonOneElement.css('display', 'inline-block');
+        buttonTwoElement.css('display', 'inline-block');
+        buttonThreeElement.css('display', 'inline-block');
+        buttonFourElement.css('display', 'inline-block');
+    } else {
+        buttonOneElement.css('display', 'none');
+        buttonTwoElement.css('display', 'none');
+        buttonThreeElement.css('display', 'none');
+        buttonFourElement.css('display', 'none');
+    }
+}
 
 function startQuiz(questionNumber) {
     console.log('Starting quiz on question number ' + (questionNumber + 1));
-    
 
 }
 
@@ -58,3 +75,13 @@ function startQuiz(questionNumber) {
 })*/
 
 resetInstructions();
+toggleAnswerButtons("on");
+
+
+
+
+/* WORKS //////////
+console.log(answersElement.children().length);
+for(i=0; i < answersElement.children().length; i++) {
+    console.log(answersElement.children().eq(i).text());
+} //////////*/
