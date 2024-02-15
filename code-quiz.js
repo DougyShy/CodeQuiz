1e498134df87bd75bf4db9a467f12acb72131384
Doug Scheible <doug_scheible@yahoo.com>
2024-02-14 20:55:15 -0600

    problems with git and 2 comps

--- code-quiz.js
+++ code-quiz.js
@@ -8,7 +8,7 @@ var buttonFourElement = $("#button-four");
 
 var answersElement = $('ul');
 
-var instructionsText = "<h1>" + "Coding Quiz Challenge</h1><h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorect answers will penalize your score/time by ten seconds!</H2>";
+var instructionsText = "<h1>" + "Coding Quiz Challenge</h1><h2>Try to answer the following code-related questions within the time limit. Keep in mind that incorrect answers will penalize your score/time by ten seconds!</H2>";
 
 questions = ["Commonly used data types DO Not Include:",
              "The condition in an if / else statement is enclosed with ____________.",
